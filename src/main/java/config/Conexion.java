81fbe748639a772f9a89c9aaac45477eb96130ed
Diego Sobarzo <sobarzolicandeo@gmail.com>
2023-05-23 14:22:53 -0400

    46 eliminacion de errores

--- src/main/java/config/Conexion.java
+++ src/main/java/config/Conexion.java
@@ -22,26 +22,3 @@ public class Conexion {
 
 
 
-/*
-
-import java.sql.DriverManager;
-
-/**
- *
- * @author docencia
- */
-/*public class Conexion {
-    Connection con;
-    String url = "jdbc:mysql://localhost:3306/bdcarritocompras";
-    String user = "root";
-    String pass = "";
-    public Connection getConnection() {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            con = (Connection) DriverManager.getConnection(url, user, pass);
-        } catch(Exception e) {
-        }
-        return con;
-    }
-}
- */
