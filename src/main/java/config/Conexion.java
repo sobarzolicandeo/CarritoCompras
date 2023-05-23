b8abfc151b1bea002be5441d7e83285f0d68b70f
Diego Sobarzo <sobarzolicandeo@gmail.com>
2023-05-23 15:23:12 -0400

    45 eliminacion de errores

--- src/main/java/config/Conexion.java
+++ src/main/java/config/Conexion.java
@@ -1,9 +1,13 @@
+
 package config;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Conexion {
+    private static final Logger logger = Logger.getLogger(Conexion.class.getName());
     private Connection con;
     private String url = "jdbc:mysql://localhost:3306/bdcarritocompras";
     private String user = "root";
@@ -14,7 +18,7 @@ public class Conexion {
             Class.forName("com.mysql.jdbc.Driver");
             con = DriverManager.getConnection(url, user, pass);
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.log(Level.SEVERE, "Error al obtener la conexión", e);
         }
         return con;
     }
